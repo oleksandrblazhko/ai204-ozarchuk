b57e788d5cb1c1fe1265a4a0bd2f4e310aad88d6
Ozrchk03 <104140571+Ozrchk03@users.noreply.github.com>
2022-11-03 21:25:35 +0200

    Update 4-Program-Methods Example.sql

--- 3-SoftwareConstruction/4-Program-Methods Example.sql
+++ 3-SoftwareConstruction/4-Program-Methods Example.sql
@@ -31,31 +31,37 @@ CREATE OR REPLACE TYPE BODY Laboratory_equipment AS
     END Laboratory_equipment;
     
     /* Процедура зміни значень атрибутів */
-	      MEMBER PROCEDURE regulateLaboratoryEquipment(p_id_labEquip INT, p_changes_in_indicators DATE, p_wf_time TIME)
+	      MEMBER PROCEDURE regulateLaboratoryEquipment(p_id_labEquip INT, p_changes_in_indicators NUMBER)
 	      IS
 	      BEGIN
-        UPDATE Weather_forecast SET wf_date = p_wf_date, wf_time = p_wf_time
-		      WHERE weather_forecast_id = p_weather_forecast_id;
-		      SELF.wf_date := p_wf_date, SELF.wf_time := p_wf_time;;
-	      END formWeatherForecast;
-   
-   
-   
-   
-   
-   
-	id_labEquip INT, -- айді
-	changes_in_indicators NUMBER(20), -- зміни показників обладнання
-	id_equipIndic INT, -- айді показників обладнання
-  /* конструктор екземплярів об'єктів класів.
-       
-    	CONSTRUCTOR FUNCTION Laboratory_equipment(p_changes_in_indicators NUMBER)
-        	RETURN SELF AS RESULT,
-		/* Процедура зміни значення атрибутів */
-		MEMBER PROCEDURE set_changes_in_indicators(p_id_labEquip NUMBER, p_changes_in_indicators NUMBER),
-    		/* Функції отримання значень атрибутів */
-		MEMBER FUNCTION get_id_labEquip RETURN INT,
-		MEMBER FUNCTION get_changes_in_indicators RETURN NUMBER,
-	/* Процедура виводу на екран значень атрибутів */
-    	MEMBER PROCEDURE display
-);
+        UPDATE Laboratory_equipment SET changes_in_indicators = p_changes_in_indicators
+		      WHERE id_labEquip = p_id_labEquip;
+		      SELF.changes_in_indicators := p_changes_in_indicators;
+	      END regulateLaboratoryEquipment;
+	      
+	/* Функції отримання значень атрибутів */
+	      /* get_id_labEquip */
+        MEMBER FUNCTION get_id_labEquip
+	        RETURN INT 
+	      IS
+        BEGIN
+          RETURN SELF.id_labEquip;
+        END get_id_labEquip;
+        
+        /* get_changes_in_indicators */
+        MEMBER FUNCTION get_changes_in_indicators
+	        RETURN NUMBER 
+	      IS
+        BEGIN
+          RETURN SELF.changes_in_indicators;
+        END get_changes_in_indicators;
+
+        /* Процедура виводу на екран значень атрибутів */
+        MEMBER PROCEDURE display 
+        IS
+        BEGIN 
+          dbms_output.put_line('id_labEquip: ' || id_labEquip);
+          dbms_output.put_line('changes_in_indicators: ' || changes_in_indicators);
+        END display;
+END; 
+/	      

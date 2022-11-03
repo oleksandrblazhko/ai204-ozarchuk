4cf1ad2e8a8e261dd7f03adde49bc595d2f16b3b
Ozrchk03 <104140571+Ozrchk03@users.noreply.github.com>
2022-11-03 21:57:32 +0200

    Update 5-Program-Methods-Using-Example.sql

--- 3-SoftwareConstruction/5-Program-Methods-Using-Example.sql
+++ 3-SoftwareConstruction/5-Program-Methods-Using-Example.sql
@@ -2,7 +2,7 @@
 DECLARE
     le1 Laboratory_equipment;
 BEGIN 
-    le1:= NEW Laboratory_equipment(TO_CHANGES('18, 25, 10','N%, N%, N%'));
+    le1:= NEW Laboratory_equipment(TO_CHANGES_OF_INDICATORS('18, 25, 10','N%, N%, N%'));
 	    dbms_output.put_line('get id_labEquip: ' || le1.get_id_labEquip);
 	    dbms_output.put_line('get changes_in_indicators: ' || le1.changes_in_indicators);
     le1.display();
@@ -14,7 +14,7 @@ DECLARE
     le1 Laboratory_equipment;
     ei1 Equipment_indicators;
 BEGIN 
-    le1:= NEW Laboratory_equipment(TO_CHANGES('18, 25, 10','N%, N%, N%'));
+    le1:= NEW Laboratory_equipment(TO_CHANGES_OF_INDICATORS('18, 25, 10','N%, N%, N%'));
     ei1:= Equipment_indicators(15, 'air humidity', 'air temperature', 'pressure', le1);
     ei1.display();
 END;
@@ -26,7 +26,7 @@ DECLARE
    ei1 Equipment_indicators;
    ah1 Air_humidity;
 BEGIN
-    le1:= NEW Laboratory_equipment(TO_CHANGES('18, 25, 10','N%, N%, N%'));
+    le1:= NEW Laboratory_equipment(TO_CHANGES_OF_INDICATORS('18, 25, 10','N%, N%, N%'));
     ei1:= Equipment_indicators(15, 'air humidity', 'air temperature', 'pressure', le1);
     ah1 := Air_humidity(20, 15.2, ei1);
     ah1.display(); 
@@ -41,7 +41,7 @@ DECLARE
     us1 User;
     us2 User;
 BEGIN 
-    le1:= NEW Laboratory_equipment(TO_CHANGES('18, 25, 10','N%, N%, N%'));
+    le1:= NEW Laboratory_equipment(TO_CHANGES_OF_INDICATORS('18, 25, 10','N%, N%, N%'));
     ei1:= Equipment_indicators(15, 'air humidity', 'air temperature', 'pressure', le1);
     ah1 := Air_humidity(20, 15.2, ei1);
     us1 := User(54, 'Ozarchuk', 'Anna', 'Sergiivna', 'anya.ozarchuk@gmail.com', '098 546 65 12', NULL, ei1, ah1);
